e5851ccb9e797e6fcf7ecd208c7c2c3ec3d317ff
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-02-15 20:01:15 -0500

    python-postgres ok

--- workspace/processor-pyclient/milvus_index_processor.py
+++ workspace/processor-pyclient/milvus_index_processor.py
@@ -18,6 +18,8 @@ import torch
 import numpy as np
 import uuid
 
+import asyncpg
+
 #gc.collect()
 #torch.cuda.empty_cache()
 
@@ -233,6 +235,29 @@ class MilvusIndexProcessor(SUPER_CLASS):
         )
         return {}
     
+    async def pgtest(self, args, default_arguments):
+        conn_params = {
+            'user': os.environ['POSTGRES_USER'],
+            'password': os.environ['POSTGRES_PASSWORD'],
+            'database': os.environ['POSTGRES_DB'],
+            'host': os.environ['POSTGRES_HOST'], 
+            'port': int(os.environ['POSTGRES_PORT'])
+        }
+
+        # Establish a connection
+        conn = await asyncpg.connect(**conn_params)
+
+        #await conn.execute('''INSERT INTO users(name, dob) VALUES($1)''', 
+        #                   'Bob')
+
+        row = await conn.fetchrow(
+        "SELECT NOW(), $1 as text", 'Bob')
+
+        print(row)
+
+        await conn.close()
+        return {}
+    
     async def process(self, args, default_arguments):
         method = args['method']
         if method == "indexqa":
@@ -243,6 +268,8 @@ class MilvusIndexProcessor(SUPER_CLASS):
             return await self.delete_qa(args, default_arguments)
         elif method == "updateqa":
             return await self.update_qa(args, default_arguments)
+        elif method == "pgtest":
+            return await self.pgtest(args, default_arguments)
 
         return {}
 
